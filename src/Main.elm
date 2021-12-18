1aecda9e0b4f73d154b07f57235f82590aec0970
Diogo Korok <korokd@protonmail.ch>
2021-12-17 21:31:35 -0300

    Apply darker background to not hurt my eyes

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser exposing (Document)
 import Element exposing (Element)
+import Element.Background
 import Model exposing (Flags, Model)
 import Msg exposing (Msg(..))
 import Ticket
@@ -39,7 +40,8 @@ view model =
     { title = "Instant Lotto with elm-ui"
     , body =
         [ Element.layout
-            []
+            [ Element.Background.color <| Element.rgba255 111 111 111 1
+            ]
           <|
             Element.row
                 [ Element.height Element.fill
