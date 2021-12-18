42ca1e1c9f2e6f1581371db1095881b1a5b212ac
Diogo Korok <korokd@protonmail.ch>
2021-12-17 21:25:30 -0300

    Create Square content

--- src/Square.elm
+++ src/Square.elm
@@ -1,11 +1,17 @@
 module Square exposing (view)
 
 import Element exposing (Element)
+import Element.Background
 import Model exposing (Model)
 import Msg exposing (Msg)
 
 
 view : Int -> Model -> Element Msg
 view _ _ =
-    -- use alignCenter here later
-    Element.none
+    Element.el
+        [ Element.centerX
+        , Element.Background.color <| Element.rgba255 255 140 0 1
+        , Element.height <| Element.px 200
+        , Element.width <| Element.px 200
+        ]
+        Element.none
